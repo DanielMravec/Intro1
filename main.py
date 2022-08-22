1b481970e05afdbfe9613cdb22ea2fe7abb6caa7
DanielMravec <111720807+DanielMravec@users.noreply.github.com>
2022-08-22 16:39:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#Adds pygame
+import pygame 
+import random
+
+#print("Hello World!")
+#name = input("What is your name?")
+#print(name)
+
+#i = 1
+
+#while i <= 10:
+  #print(i)
+  #i += 1
+
+#for x in range(1, 10):
+  #print(x)
+
+print(random.randint(1, 100))
+print(random.randrange(1, 100))
